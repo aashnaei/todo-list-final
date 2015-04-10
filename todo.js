4e71842aed16e6606eb156c0fafdcc31682acf5b
adel ashnaei <aashnaei@gmail.com>
2015-04-09 17:16:27 -0700

    bower

--- todo.js
+++ todo.js
@@ -147,48 +147,84 @@ myapp.controller('TodoCtrl', function ($scope) {
 
 	};
 
-angular.module('drag', []).
-directive('draggable', function($document) {
-  return function(scope, element, attr) {
-    var startX = 0, startY = 0, x = 0, y = 0;
-    element.css({
-     position: 'relative',
-     border: '1px solid red',
-     backgroundColor: 'lightgrey',
-     cursor: 'pointer',
-     display: 'block',
-     width: '65px'
-    });
-    element.on('mousedown', function(event) {
-      // Prevent default dragging of selected content
-      event.preventDefault();
-      startX = event.screenX - x;
-      startY = event.screenY - y;
-      $document.on('mousemove', mousemove);
-      $document.on('mouseup', mouseup);
-    });
+// angular.module('drag', []).
+// directive('draggable', function($document) {
+//   return function(scope, element, attr) {
+//     var startX = 0, startY = 0, x = 0, y = 0;
+//     // element.css({
+//     //  position: 'relative',
+//     //  border: '1px solid red',
+//     //  backgroundColor: 'lightgrey',
+//     //  cursor: 'pointer',
+//     //  display: 'block',
+//     //  width: '65px'
+//     // });
+//     element.on('mousedown', function(event) {
+//       // Prevent default dragging of selected content
+//       event.preventDefault();
+//       startX = event.screenX - x;
+//       startY = event.screenY - y;
+//       $document.on('mousemove', mousemove);
+//       $document.on('mouseup', mouseup);
+//     });
 
-    function mousemove(event) {
-      y = event.screenY - startY;
-      x = event.screenX - startX;
-      element.css({
-        top: y + 'px',
-        left:  x + 'px'
-      });
-    }
+//     function mousemove(event) {
+//       y = event.screenY - startY;
+//       x = event.screenX - startX;
+//       element.css({
+//         top: y + 'px',
+//         left:  x + 'px'
+//       });
+//     }
 
-    function mouseup() {
-      $document.off('mousemove', mousemove);
-      $document.off('mouseup', mouseup);
-    }
-  };
-});
+//     function mouseup() {
+//       $document.off('mousemove', mousemove);
+//       $document.off('mouseup', mouseup);
+//     }
+//   };
+// });
 
 
 	// setTimeout(addDrag, 500);
-});
+})
 
+  .directive('myDraggable', ['$document', function($document) {
+    return {
+      link: function(scope, element, attr) {
+        var startX = 0, startY = 0, x = 0, y = 0;
 
+        // element.css({
+        //  position: 'relative',
+        //  border: '1px solid red',
+        //  backgroundColor: 'lightgrey',
+        //  cursor: 'pointer'
+        // });
+
+        element.on('mousedown', function(event) {
+          // Prevent default dragging of selected content
+          event.preventDefault();
+          startX = event.pageX - x;
+          startY = event.pageY - y;
+          $document.on('mousemove', mousemove);
+          $document.on('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+          y = event.pageY - startY;
+          x = event.pageX - startX;
+          element.css({
+            top: y + 'px',
+            left:  x + 'px'
+          });
+        }
+
+        function mouseup() {
+          $document.off('mousemove', mousemove);
+          $document.off('mouseup', mouseup);
+        }
+      }
+    };
+  }]);
 
 
 // var elements = document.getElementById('dragMe');
@@ -220,7 +256,6 @@ directive('draggable', function($document) {
 //   }
 
 // };
-
 
 
 
